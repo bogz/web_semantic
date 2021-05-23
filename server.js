c184a697b42b028e3ad3ab071be2b6cc322a0538
Bogdan Orbai <bogdan.orbai@tora.com>
2021-05-23 12:58:19 +0300

    Added insert and delete

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 const app = express()
 const port = 5000
-const URL = 'http://localhost:3000';
+const GRAPHQL_URL = 'http://localhost:3000';
 
 // fix cors issue
 app.use(cors())
@@ -30,7 +30,7 @@ app.get('/get-data', async (req, res) => {
                   }
               }`
     const response = await axios({
-        url: URL,
+        url: GRAPHQL_URL,
         method: 'POST',
         data: {query: query}
     })
@@ -44,20 +44,65 @@ app.get('/get-data', async (req, res) => {
 
 app.delete('/delete-song', async(req, res) => {
     const songIdToDelete = Number(req.body.songId);
-    const mutation = `
-                        mutation {
+    const removeSongMutation = `mutation {
                             removeSong(id:${songIdToDelete}) {
                                 name
                             }
                         }`
     const response = await axios({
-        url: URL,
+        url: GRAPHQL_URL,
         method: 'POST',
-        data: {query: mutation}
+        data: {query: removeSongMutation}
     });
 
     if (response.status === 200) {
         res.status(200).send(JSON.stringify(response.data.data));
+    } else {
+        res.sendStatus(500)
+    }
+})
+
+app.post('/add-song', async(req, res) => {
+    const data = req.body.data;
+    const genreName = '"' + data.genreName + '"';
+    const genreIdQuery = `
+                          {
+                            allGenres(filter: {name: ${genreName}}) {
+                                id
+                            }
+                          }`
+
+    const genreIdResponse = await axios({
+        url: GRAPHQL_URL,
+        method: 'POST',
+        data: {query: genreIdQuery}
+    })
+
+    if (genreIdResponse.status === 200) {
+        const genreId = Number(genreIdResponse.data.data.allGenres[0].id);
+        const name = '"' + data.name + '"';
+        const artist = '"' + data.artist + '"';
+        const photo = '"images/no.jpg"';
+        const addSongMutation = `
+                            mutation {
+                                        createSong(name: ${name}, artist: ${artist}, duration: ${data.duration},
+                                         isHit: ${data.isHit}, isSingle: ${data.isSingle}, photo: ${photo}, 
+                                         genre_id: ${genreId}) {
+                                            name
+                                            artist
+                                            }
+                                     }`
+        const response = await axios({
+            url: GRAPHQL_URL,
+            method: 'POST',
+            data: {query: addSongMutation}
+        })
+
+        if (response.status === 200) {
+            res.status(200).send(JSON.stringify(response.data.data));
+        } else {
+            res.sendStatus(500)
+        }
     } else {
         res.sendStatus(500)
     }
